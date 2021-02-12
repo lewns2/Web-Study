c765248e2a3533c10eb13742f1e1987709c58956
lewns2 <lewns2@naver.com>
2021-02-12 17:36:01 +0900

    'modifeied'

--- Clone_01/todo.js
+++ Clone_01/todo.js
@@ -2,31 +2,33 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 toDoInput = toDoForm.querySelector("input"),
 toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = 'toDos';
+
 
 function paintToDo(text){
   const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  dleBtn.value = "X";
+  const del = document.createElement("button");
+  del.innerHTML = "X";
   const span = document.createElement("span");
   span.innerText = text;
   li.appendChild(span);
-  li.appendChild(delBtn);
-  toDoList.appendChild(li);
+  li.appendChild(del);
 
+  toDoList.appendChild(li);
 }
 
 function handleSubmit(event){
   event.preventDefault();
   const currentValue = toDoInput.value;
+  paintToDo(currentValue);
   toDoInput.value = "";
-
 }
+
+const TODOS_LS = 'toDos';
 
 function loadToDos(){
   const toDos = localStorage.getItem(TODOS_LS);
   if(toDos !== null){
-
+    toDoForm.addEventListener("submit", handleSubmit);
   }
 }
 
